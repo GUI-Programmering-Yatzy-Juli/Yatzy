0b4da3967fa5e75982f4ce79efce7bb9efa792de
lolbersk <31921634+lolbersk@users.noreply.github.com>
2017-09-19 14:50:05 +0200

    ui igen

--- Yatzy/Yatzy/mainwindow.cpp
+++ Yatzy/Yatzy/mainwindow.cpp
@@ -11,6 +11,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->tableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
 }
 
 MainWindow::~MainWindow()
